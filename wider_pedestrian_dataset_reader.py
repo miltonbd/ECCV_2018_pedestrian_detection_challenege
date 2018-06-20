b41bf479c87361a82c316ca57fdd690b8ed33835
miltonbd <milton.2002@yahoo.com>
2018-06-20 09:32:11 +0600

    only train example with ground truth will be added for training.

--- wider_pedestrian_dataset_reader.py
+++ wider_pedestrian_dataset_reader.py
@@ -10,21 +10,24 @@ Labels 	46513 	19696
 """
 
 def read_train_gt():
-    annotations={}
+    annotations=[]
     with open(train_bbx_gt_file,'r') as train_bbx_file:
         content=train_bbx_file.readlines();
         for line in content:
             line_list=line.split(" ")
-            # print(len(line_list))
             file_name=line_list[0]
-            annotations[file_name]=[]
+            row=[]
             for idx in range(1,len(line_list)-1,5):
                 class_num=line_list[idx]
                 left=line_list[idx+1]
                 top=line_list[idx+2]
                 w=line_list[idx+3]
-                h=line_list[idx+4]
-                annotations[file_name].append([class_num,left,top,w,h])
+                h=line_list[idx+4].strip()
+                obj=[class_num, left, top, w, h]
+                if len(obj)>0:
+                    row+=obj
+            if len(row)>0:
+                annotations.append([file_name,row[:]])
     return annotations
 
 
@@ -36,7 +39,7 @@ def read_val_gt():
             line_list=line.split(" ")
             # print(len(line_list))
             file_name=line_list[0]
-            annotations[file_name]=[]
+            annotations=[]
             for idx in range(1,len(line_list)-1,5):
                 class_num=line_list[idx]
                 left=line_list[idx+1]
@@ -57,3 +60,8 @@ def read_val_gt():
 # # annos= read_train_gt()
 # # for anno in annos:
 # #     print(annos[anno])
+
+if __name__ == '__main__':
+    train_gt=read_train_gt()
+    for row in train_gt:
+        print(row)
